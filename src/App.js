6224af5fc1dd146c784b1aea0b5ccaaab8ece174
FerhatKarad <92120049+FerhatKarad@users.noreply.github.com>
2022-09-09 00:06:25 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './logo.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <p>
+          <code></code>  Liquor Mara
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Logo} className="App-logo" alt="logo" />
+        
+        <p className='small'>1000 ml
+          Stärkungsmittel / Ergänzungsfuttermittel
+          Präparat zur Zucht und Aufzucht
+          fördert Vitalität, Kondition und Wohlbefinden
+          dient dem Knochenaufbau
+          für Kaninchen, Nagetiere und Geflügel
+          Gehalt von Inhaltsstoffen: Feuchtigkeit 60,9 %, Saccharose 13,46 %, Fructose 14,17 %, Arabinose 1,96 %, Natrium 2,03 %</p>
+
+        <p className='small'>Ferro-Prodol ist ein Nahrungsergänzungs- und Stärkungsmittel. Es enthält Organisches Eisen und gebundenen Sauerstoff. Dieses Präparat wird zur Zucht und Aufzucht verwendet, es steigert die Vitalität, Kondition und das Wohlbefinden der Tiere. Außerdem dient es dem Knochenaufbau. Dieses Mittel kann Kaninchen, Nagern und Geflügel verabreicht werden.
+ <br/>
+          Gehalt von Inhaltsstoffen: Feuchtigkeit 60,9 %, Saccharose 13,46 %, Fructose 14,17 %, Arabinose 1,96 %, Natrium 2,03 %
+          <br/>
+          Inhalt: 1000 ml
+          <br/>
+          Dosierungsempfehlung:
+          <br/>
+          Anwendung:
+          Brieftauben bekommen 5 g regelmäßig (täglich) auf 1l Wasser oder über Futter verabreicht,
+          Vor, während und nach Leistungsanforderungen (Zucht, Reise, Mauser) werden 10 g Ferro Prodol täglich auf 1 l Wasser oder über Futter verabreicht
+          <br/>
+          Kaninchen - Zwergrasse: 5 g (1 Teelöffel) für ca. 5 Tiere
+          Kaninchen - kleine Rassen: 5 g (1 Teelöffel) für ca. 3 Tiere
+          Kaninchen - mittlere und große Rassen: 2,5 g (½ Teelöffel) pro Tier
+          - Diese Dosis bitte regelmäßig (d.h. täglich auf 1 l Trinkwasser oder ins Futter gemischt verabreichen.
+          Ferro-Prodol N ist auch unentbehrlich für Jungkaninchen. Für diese die Dosis der Zwergrasse anwenden.
+          <br/>
+
+          Hühner und sonstiges Geflügel: 5 - 10 g (5 g = 1 Teelöffel) für ca. 10 Tiere regelmäßig (d.h. täglich) auf 1 l Trinkwassser oder unter das Futter mischen.
+
+          Küken: 2,5 - 5 g (2,5 g = ½ Teelöffel) für ca. 20 Tiere. Bei geschwächten Tieren 2x täglich 3 - 5 Tropfen Ferro-Prodol N unverdünnt eingeben.
+
+
+
+
+          2,5 g (½ Teelöffel) pro Tier. Diese Dosis bitte regelmäßig (d.h. täglich auf 1 l Trinkwasser oder ins Futter gemischt verabreichen.</p>
       </header>
     </div>
   );
